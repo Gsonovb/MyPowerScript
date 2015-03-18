0d06b566c3a6a36112c88487f76d20cd16b2d704
Guanyc <guanyc@gmail.com>
2015-03-18 22:05:24 +0800

    添加 Azure 脚本
    增加用于 从 csv 批量添加/删除 Azure Endpoint 的脚本

---
+++ Azure/Remove-AzureVMAllEndpoints.ps1
@@ -0,0 +1,25 @@
+﻿# Arguments.
+param 
+(
+	[Microsoft.WindowsAzure.Commands.ServiceManagement.Model.ServiceOperationContext]$vm = $(throw "'vm' 参数是必须的。")
+)
+
+
+$allendpoints=Get-AzureEndpoint -VM $vm
+
+foreach($ep in $allendpoints)
+{
+   if( ($ep.Name -ine "Powershell") -and ($ep.Name -ine "Remote Desktop") -and ($ep.Name -ine "SSH"))
+   {
+     write-host "Deleting EndPoint "$ep.Name
+     $r=  Remove-AzureEndpoint -VM $vm -Name $ep.Name
+   }
+}
+
+
+
+
+# Update VM.
+Write-Host -Fore Green "更新 虚机..."
+$vm | Update-AzureVM 
+Write-Host -Fore Green "完成."
