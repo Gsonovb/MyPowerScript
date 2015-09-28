7621cdd8439d5a4f04ba9d965f6f89538364a46d
Guanyc <guanyc@gmail.com>
2015-09-28 12:21:25 +0800

    添加新的Channel9下载脚本
    添加新的Channel9下载脚本

---
+++ Channel9/DownloadNuGetPackage.psm1
@@ -0,0 +1,160 @@
+﻿function DownloadNuGetPackage
+{
+    <#
+    .Synopsis
+       下载 Nuget 包并返回类库路径
+    .DESCRIPTION
+       下载 Nuget 包并返回类库路径
+    .EXAMPLE
+       DownloadNuGetPackage -PackageName "HtmlAgilityPack" 
+       
+    .EXAMPLE
+       Import-Module .\DownloadNuGetPackage.psm1
+       DownloadNuGetPackage -PackageName "HtmlAgilityPack" -NetVersion Net40 -WorkingDirectory "g:\temp" -Force
+
+    .NOTES
+       Copyright  Guanyc  All rights reserved.
+    .FUNCTIONALITY
+       检查 Nuget 文件是否存在，并使用NuGet.exe 获取 NuGet Gallery 组件包，并返回要加载的类库文件路径。
+
+    .PARAMETER PackageName
+        要安装包的名称
+
+    .PARAMETER nugetUrl
+        程序的下载连接
+
+    .PARAMETER WorkingDirectory
+        工作文件夹 用于保存下载程序包的位置
+
+    .PARAMETER Force
+        指示是否强制更新 Nuget 文件
+
+    .PARAMETER NetVersion
+        返回 对应 .NET 版本的类库 
+
+
+    #>
+
+    [CmdletBinding(DefaultParameterSetName="BASE")] #,
+    #HelpURI="http://gallery.technet.microsoft.com/scriptcenter/Convert-WindowsImageps1-0fe23a8f")]
+
+    Param
+    (
+        # 要安装包的名称
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   ValueFromRemainingArguments=$false, 
+                   Position=0,
+                   ParameterSetName='BASE')]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $PackageName,
+
+        # nuget 程序的下载连接
+        [Parameter(ParameterSetName='BASE',Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe",
+
+        #工作文件夹 用于保存下载程序包的位置
+        [Parameter(ParameterSetName="BASE")]
+        [Alias("WorkDir")]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $WorkingDirectory = $pwd ,
+        
+        # 返回 对应 .NET 版本的类库 
+        [Parameter(ParameterSetName='BASE')]
+        [Alias("NetVer")]
+        [String]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("Net20","Net40","Net45","NetCore45","All")]
+        $NetVersion = "NET45",
+
+        #指示是否强制更新 Nuget 文件
+        [Parameter(ParameterSetName="BASE")]
+        [Switch]
+        $Force = $False
+
+    )
+
+    Begin
+    {
+
+
+    }
+    Process
+    {
+        
+        if( -not ([String]::IsNullOrEmpty($WorkingDirectory) )) {
+            if( -not (Test-Path -Path $WorkingDirectory)) { $r = md $WorkingDirectory}
+            $path = $WorkingDirectory
+        }
+        elseif(Test-Path -Path $PSScriptRoot){
+            $path = $PSScriptRoot
+        }
+        else{
+            $path = Get-Location
+        }
+
+
+        $filename = Join-Path -Path $path -ChildPath "nuget.exe"
+
+        Write-Host "Find Nuget.exe in $path "
+
+ 
+        $webClient = New-Object System.Net.WebClient 
+        
+        $r = Register-ObjectEvent -InputObject $webClient -EventName DownloadProgressChanged -SourceIdentifier WebClient.DownloadProgressChanged -Action { Write-Progress -Activity "Downloading:  $($EventArgs.BytesReceived) of $($EventArgs.TotalBytesToReceive) bytes." -PercentComplete $EventArgs.ProgressPercentage;  }    
+        $r = Register-ObjectEvent -InputObject $webClient -EventName DownloadFileCompleted -SourceIdentifier WebClient.DownloadFileComplete -Action {  Write-Host "Download Complete. ";  }  
+        
+
+        if( ( -not (Test-Path -Path $filename)) -or $Force ){
+            Write-Host  "Can't Find Nuget.exe, so download it from url $nugetUrl ."  
+
+           
+            $webclient.DownloadFileAsync($nugetUrl,$filename)
+
+            do{sleep -Seconds 1 } while ( $webClient.IsBusy)
+
+            write-progress -activity "Downloading Nuget"  -percentcomplete 100 -Completed 
+        }
+        else{
+            Write-Host "Finded Nuget.exe file."
+        }
+        
+        Unregister-Event -SourceIdentifier WebClient.DownloadProgressChanged; Unregister-Event -SourceIdentifier WebClient.DownloadFileComplete; 
+
+        Set-Alias -Name nuget -Value $filename
+
+        cd $path
+
+        $r = nuget install $PackageName
+
+        Write-Host $r
+
+        $packagedir = Get-ChildItem  -Path $path -Directory  |Where-Object -Property Name -Like ("*" + $PackageName + "*") | Select-Object -Last 1
+
+        $matchstr = "\\net"
+        
+        if( $NetVersion -eq "Net20") {$matchstr = "\\Net20"}
+        if( $NetVersion -eq "Net40") {$matchstr = "\\Net40"}
+        if( $NetVersion -eq "Net45") {$matchstr = "\\Net45"}
+        if( $NetVersion -eq "NetCore45") {$matchstr = "\\NetCore45"}
+
+
+        $dllfile = Get-ChildItem  -Path $packagedir -Include "*.dll" -Recurse | Where-Object -Property  DirectoryName -Match $matchstr | Select-Object -First 1
+
+        Write-Host "Return File: " $dllfile.FullName
+        return $dllfile.FullName
+
+
+    }
+    End
+    {
+    }
+
+}
+
+
---
+++ Channel9/channel9download.ps1
@@ -0,0 +1,309 @@
+Add-Type -AssemblyName System.Web
+
+$langs = ("en","zh-cn")
+
+$overwrite = $false
+
+$Quality = ("HIGH","MID","LOW")
+$VideoType = ("MP4","WMV")
+
+
+$dir = get-location
+
+
+
+
+$urls=("https://channel9.msdn.com/Series/IntroToAD") #,"https://channel9.msdn.com/Series/ConnectOn-Demand"
+
+$ScriptFileName = "DownloadNuGetPackage.psm1"
+$RequesetAssembliy ="HtmlAgilityPack.dll"
+
+$nugetScript = "F:\temp\DownloadNuGetPackage.psm1"
+
+
+if( [String]::IsNullOrEmpty($PSScriptRoot)){
+    $filename = Join-Path -Path $PWD -ChildPath $RequesetAssembliy
+}
+else
+{
+    $filename = Join-Path -Path $PSScriptRoot -ChildPath $RequesetAssembliy
+}
+
+if(Test-Path -Path $filename){  
+}
+else
+{
+
+    if(Test-Path -Path $nugetScript){
+        
+    }elseif([String]::IsNullOrEmpty($PSScriptRoot)){
+        $nugetScript = Join-Path -Path $PWD -ChildPath $ScriptFileName
+    }
+    else{
+        $nugetScript = Join-Path -Path $PSScriptRoot -ChildPath $ScriptFileName
+    }
+
+    if( -not (Test-Path -Path $nugetScript)){  
+        #Download script   
+    }
+
+    Import-Module  $nugetScript
+
+    $libfile =  DownloadNuGetPackage -PackageName "HtmlAgilityPack" 
+
+    Copy-Item -Path $libfile -Destination $filename  
+
+    $filename = $libfile
+
+}
+
+
+Add-Type -Path $filename
+
+
+
+
+#清理文件名中的无效字符
+function RemoveInvalidChars ($filename)
+{
+    
+    foreach($item in [System.IO.Path]::GetInvalidFileNameChars())
+    {
+	    $filename=$filename.Replace($item ,"_")
+    }
+
+    foreach($item in [System.IO.Path]::GetInvalidPathChars())
+    {
+	    $filename=$filename.Replace($item ,"_")
+    }
+    
+    return $filename
+}
+
+
+
+
+$htmldoc = New-Object HtmlAgilityPack.HtmlDocument
+$webClient = New-Object System.Net.WebClient 
+$webClient.Encoding=[System.Text.Encoding]::UTF8
+
+
+
+
+$linkfeeds = New-Object -TypeName System.Collections.ArrayList
+
+
+#获取所有下载页面的连接
+foreach($url in $urls){
+
+
+    Write-Host "Get  $url ..."
+
+    $pagecontent = $webClient.DownloadString($url)
+    $htmldoc.LoadHTML($pagecontent)
+
+    $titlestr  = $htmldoc.DocumentNode.SelectSingleNode("//h1") | Select-Object -ExpandProperty InnerText
+
+    if(-not [string]::IsNullOrEmpty($titlestr)) 
+    {
+        $title  = [System.Web.HttpUtility]::HtmlDecode($titlestr)
+    }
+
+
+    $baselink = ([System.Uri]$url)
+
+    $links = $htmldoc.DocumentNode.SelectNodes('//a[@class="title"]') |Select-Object -ExpandProperty Attributes | Where-Object -Property Name -EQ "href" | Select-Object -ExpandProperty Value
+        
+  
+    $hasnextpage = $false
+
+    do
+    {
+        Write-Host "find links Count: " $links.Count
+        
+        foreach($link in $links)
+        {
+            $linkurl = New-Object -Typename "System.Uri" ($baselink , [System.Web.HttpUtility]::UrlDecode($link))
+    
+            $feed = [PSCustomObject][Ordered]@{SerieName = $title;
+                                               Url   = $linkurl}
+
+            $r=$linkfeeds.Add($feed)
+        }
+
+        
+        
+        $nextstr = $htmldoc.DocumentNode.SelectSingleNode('//li[@class="next"]/a') |Select-Object -ExpandProperty Attributes | Where-Object -Property Name -EQ "href" | Select-Object -ExpandProperty Value
+        
+        $hasnextpage = -not [String]::IsNullOrEmpty($nextstr)
+
+        if( $hasnextpage ){
+
+            $nextlink = [System.Web.HttpUtility]::UrlDecode($nextstr)
+            $linkurl = New-Object -Typename "System.Uri" ($baselink ,  $nextlink)
+
+            Write-Host "Get Next Page $linkurl ..."
+
+
+            $pagecontent = $webClient.DownloadString($linkurl)
+            $htmldoc.LoadHTML($pagecontent)
+
+            $links = $htmldoc.DocumentNode.SelectNodes('//a[@class="title"]') |Select-Object -ExpandProperty Attributes | Where-Object -Property Name -EQ "href" | Select-Object -ExpandProperty Value
+     
+        }       
+
+    }
+    while ($hasnextpage)
+    
+
+}
+
+
+
+
+
+#要下载连接，以及保存文件名
+
+$downloadfeeds = New-Object -TypeName System.Collections.ArrayList
+
+
+foreach($lf in $linkfeeds){
+    
+#    $lf = $linkfeeds[0]
+
+    $url = $lf.Url
+        
+    Write-Host "Get download links from $url  ..."
+
+    $pagecontent = $webClient.DownloadString($url)
+    $htmldoc.LoadHTML($pagecontent)
+        
+    $titlestr = $htmldoc.DocumentNode.SelectSingleNode("//h1") | Select-Object -ExpandProperty InnerText
+
+    if(-not [string]::IsNullOrEmpty($titlestr)) {
+        $title  = [System.Web.HttpUtility]::HtmlDecode($titlestr)
+    }
+
+    
+    $name=$title.Substring($title.LastIndexOf(":")+1).Trim()
+		    
+    $name = RemoveInvalidChars($name)
+		
+
+    $links =  $htmldoc.DocumentNode.SelectNodes('//*[@id="video-download"]/ul/li/div/a') |Select-Object -ExpandProperty Attributes | Where-Object -Property Name -EQ "href" | Select-Object -ExpandProperty Value 
+
+    foreach($vt in $VideoType){
+        $parts =  $links | Select-String -Pattern $vt
+        
+        $link = $null
+        
+        if([String]::IsNullOrEmpty($link) -and $Quality.Contains("HIGH")){
+            $link = $parts | Select-String -Pattern "high|source" | Select-Object -First 1
+        }
+        if([String]::IsNullOrEmpty($link) -and $Quality.Contains("MID")){
+             $link = $parts | Select-String -Pattern "mid|\.wmv" | Select-Object -First 1
+        }
+        if([String]::IsNullOrEmpty($link) -and $Quality.Contains("LOW")){
+             $link = $parts | Select-String -Pattern "\.mp4" | Select-Object -First 1
+        }
+
+        if([String]::IsNullOrEmpty($link)){
+            Write-Warning "未找到匹配的类型链接"
+        }
+        else{
+             $linkurl = [System.Web.HttpUtility]::UrlDecode($link)
+             $filename = $name + [System.IO.Path]::GetExtension($linkurl)
+             $feed = [PSCustomObject][Ordered]@{SerieName = $lf.SerieName;
+                                                Filename  = $filename;
+                                                   Url    = $linkurl;
+                                                   Type   = 0 }
+             $r=$downloadfeeds.Add($feed)
+        }
+
+    }
+
+         
+    foreach($lang in $langs)
+    {
+        $linkurl = $url.ToString() + "/captions?f=webvtt&l=$lang"
+        $filename = "$name.$lang.vtt"
+        $feed = [PSCustomObject][Ordered]@{SerieName = $lf.SerieName;
+                                           Filename  = $filename;
+                                              Url    = $linkurl;
+                                              Type   = 1 }
+        $r=$downloadfeeds.Add($feed)
+    }
+ 
+}
+
+
+
+
+
+#下载文件
+$feeds =  $downloadfeeds |Sort-Object –Property Type -Descending
+
+
+
+#$webClient = New-Object System.Net.WebClient 
+#$webClient.Encoding=[System.Text.Encoding]::UTF8
+
+
+$entries = $feeds.Length
+$progress = 0 
+$pagepercent = 0 
+
+#Unregister-Event -SourceIdentifier WebClient.DownloadProgressChanged; Unregister-Event -SourceIdentifier WebClient.DownloadFileComplete; 
+
+Register-ObjectEvent -InputObject $webClient -EventName DownloadProgressChanged -SourceIdentifier WebClient.DownloadProgressChanged -Action { Write-Progress -Activity "Downloading:  $($EventArgs.BytesReceived) of $($EventArgs.TotalBytesToReceive) bytes. $($EventArgs.ProgressPercentage)% Completed" -Status $name -PercentComplete $EventArgs.ProgressPercentage -id 10 -ParentId 1;  }    
+Register-ObjectEvent -InputObject $webClient -EventName DownloadFileCompleted -SourceIdentifier WebClient.DownloadFileComplete -Action {  Write-Host "Download Complete - $name";  }  
+
+
+
+foreach ($item in $feeds){
+    $serie = RemoveInvalidChars($item.SerieName)
+    $name  = $item.Filename
+    $link  = $item.Url
+
+    $path = Join-Path -Path $dir -ChildPath $serie
+
+    if(-not (Test-Path $path)){
+        $r= md $path
+    }
+    
+    $saveFileName = Join-Path -Path  $path  -ChildPath $name
+
+
+    if ((-not $overwrite) -and (Test-Path -path $saveFileName))     
+    {        
+        write-progress -activity "$saveFileName already downloaded" -status "$pagepercent% ($progress / $entries) complete" -percentcomplete $pagepercent    -id 1
+    }    
+    else     
+    {        
+        write-progress -activity "Downloading $saveFileName" -status "$pagepercent% ($progress / $entries) complete" -percentcomplete $pagepercent    -Id 1   
+        &{#TRY
+
+            $webClient.DownloadFileAsync($link, $saveFileName)
+
+
+            do 
+            {
+                 #$webClient.IsBusy
+                sleep -Seconds 5
+            } while ( $webClient.IsBusy)
+            
+            
+            #getfile -url  $link  -filename $saveFileName
+        }
+        trap [Exception]{
+            write-host
+            write-host ("Unable to download " + $saveFileName)
+            continue; 
+        }
+    }    
+    $pagepercent = [Math]::floor((++$progress)/$entries*100) 
+}
+
+Write-Progress -Activity "Done" -Completed -Id 1
+
+
